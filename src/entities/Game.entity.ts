d76982dca970f4f5245ef8a0e75a3e08d0a08977
Male29003 <nam2309200312345@gmail.com>
2025-11-06 01:53:27 +0700

    thêm ràng buộc User-Game

--- src/entities/Game.entity.ts
+++ src/entities/Game.entity.ts
@@ -1,23 +1,33 @@
-import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from 'typeorm';
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  OneToMany,
+  ManyToMany,
+} from 'typeorm';
 import type { Offer } from './Offer.entity.js';
+import { User } from './User.entity.js';
 
 @Entity()
 export class Game {
-    @PrimaryGeneratedColumn('uuid')
-    id: string;
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
 
-    @Column({ type: 'varchar', length: 255 })
-    name: string;
+  @Column({ type: 'varchar', length: 255 })
+  name: string;
 
-    @Column({ type: 'text', nullable: true })
-    description?: string;
+  @Column({ type: 'text', nullable: true })
+  description?: string;
 
-    @Column({ type: 'simple-array' , nullable: true })
-    platforms: string[];
+  @Column({ type: 'simple-array', nullable: true })
+  platforms: string[];
 
-    @Column({ type: 'varchar', length: 50, nullable: true })
-    type?: string;
+  @Column({ type: 'varchar', length: 50, nullable: true })
+  type?: string;
 
-    @OneToMany('Offer', 'game', { cascade: true, eager: true })
-    offers: Offer[];
+  @OneToMany('Offer', 'game', { cascade: true, eager: true })
+  offers: Offer[];
+
+  @ManyToMany(() => User, (user) => user.favoriteGames)
+  favoredByUsers: User[];
 }
